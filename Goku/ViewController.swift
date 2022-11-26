344fa1fd4c6d6bd39da81f29d72c61eb83113f24
HanYuan <max71239977@gmail.com>
2022-11-27 00:12:09 +0800

    First Commit

--- Goku/ViewController.swift
+++ Goku/ViewController.swift
@@ -8,12 +8,23 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var gifImageView: UIImageView!
+    @IBOutlet weak var slider: UISlider!
+    @IBOutlet weak var imageView: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        playGif()
     }
-
-
+    
+    func playGif()  {
+        let animatedImage = UIImage.animatedImageNamed("Goku-", duration: 4)
+        gifImageView.image = animatedImage
+    }
+    
+    @IBAction func gifSlider(_ sender: Any) {
+        let imageName = String(format: "Goku-%d", arguments: [Int(slider.value)])
+        imageView.image = UIImage(named: imageName)
+    }
 }
 
